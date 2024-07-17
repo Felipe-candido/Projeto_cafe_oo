7c75d190387c3d652d136b27d373a27289e17fb5
Felipe-candido <felipepcandido8@gmail.com>
2024-07-17 01:57:00 -0300

    inicio da classe conexão com o banco

--- classes/classe_cadastro.php
+++ classes/classe_cadastro.php
@@ -1,3 +1,21 @@
-class cadastro{
-    
+<?php
+
+class dbconnection
+{
+    private $host = 'localhost';
+    private $dbname = 'cafe';
+    private $user = 'root';
+    private $senha = '';
+    private $pdo;
+
+    public function __construct()
+    {
+        try{
+            $this->pdo = new PDO()
+        }
+    }
 }
+
+
+
+?>
